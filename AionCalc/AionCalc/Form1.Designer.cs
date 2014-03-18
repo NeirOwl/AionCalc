8b15c00f8d27766b10f4405cdab62cf5f7756482
Osipov944 <lexus_7456951@mail.ru>
2014-03-18 14:18:08 +0700

    Created first operations

---
+++ AionCalc/AionCalc/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AionCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstArgument = new System.Windows.Forms.TextBox();
+            this.SecondArgument = new System.Windows.Forms.TextBox();
+            this.Answer = new System.Windows.Forms.TextBox();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Subtraction = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstArgument
+            // 
+            this.FirstArgument.Location = new System.Drawing.Point(71, 66);
+            this.FirstArgument.Name = "FirstArgument";
+            this.FirstArgument.Size = new System.Drawing.Size(229, 20);
+            this.FirstArgument.TabIndex = 0;
+            // 
+            // SecondArgument
+            // 
+            this.SecondArgument.Location = new System.Drawing.Point(71, 124);
+            this.SecondArgument.Name = "SecondArgument";
+            this.SecondArgument.Size = new System.Drawing.Size(229, 20);
+            this.SecondArgument.TabIndex = 1;
+            // 
+            // Answer
+            // 
+            this.Answer.Location = new System.Drawing.Point(71, 179);
+            this.Answer.Name = "Answer";
+            this.Answer.ReadOnly = true;
+            this.Answer.Size = new System.Drawing.Size(229, 20);
+            this.Answer.TabIndex = 2;
+            // 
+            // Addition
+            // 
+            this.Addition.Location = new System.Drawing.Point(71, 287);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(49, 47);
+            this.Addition.TabIndex = 3;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = true;
+            this.Addition.Click += new System.EventHandler(this.Addition_Click);
+            // 
+            // Subtraction
+            // 
+            this.Subtraction.Location = new System.Drawing.Point(140, 287);
+            this.Subtraction.Name = "Subtraction";
+            this.Subtraction.Size = new System.Drawing.Size(49, 47);
+            this.Subtraction.TabIndex = 4;
+            this.Subtraction.Text = "-";
+            this.Subtraction.UseVisualStyleBackColor = true;
+            this.Subtraction.Click += new System.EventHandler(this.Subtraction_Click);
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.Location = new System.Drawing.Point(206, 287);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(49, 47);
+            this.Multiplication.TabIndex = 5;
+            this.Multiplication.Text = "*";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            this.Multiplication.Click += new System.EventHandler(this.Multiplication_Click);
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(272, 287);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(49, 47);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 384);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Subtraction);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.SecondArgument);
+            this.Controls.Add(this.FirstArgument);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstArgument;
+        private System.Windows.Forms.TextBox SecondArgument;
+        private System.Windows.Forms.TextBox Answer;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Subtraction;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Division;
+    }
+}
+
---
+++ AionCalc/AionCalc/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AionCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addition_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(FirstArgument.Text);
+            double second = Convert.ToDouble(SecondArgument.Text);
+            Adder calc = new Adder();
+            Answer.Text = calc.Calculate(first, second);
+        }
+
+        private void Subtraction_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(FirstArgument.Text);
+            double second = Convert.ToDouble(SecondArgument.Text);
+            Subtraction calc = new Subtraction();
+            Answer.Text = calc.Calculate(first, second);
+        }
+
+        private void Multiplication_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(FirstArgument.Text);
+            double second = Convert.ToDouble(SecondArgument.Text);
+            Multiplication calc = new Multiplication();
+            Answer.Text = calc.Calculate(first, second);
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(FirstArgument.Text);
+            double second = Convert.ToDouble(SecondArgument.Text);
+            Division calc = new Division();
+            Answer.Text = calc.Calculate(first, second);
+        }
+    }
+}
