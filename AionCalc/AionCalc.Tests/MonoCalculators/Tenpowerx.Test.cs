9d6016b87fbed74c2a1b230bea241451719e0927
aionforever <Tujh95@yandex.ru>
2014-04-14 18:57:47 +0700

    Added Tenpowerx.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Tenpowerx.Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Tenpowerx
+    {
+        [Test]
+        public void TenpowerxTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("10^x");
+            double result = calc.Calculate(2);
+            Assert.AreEqual(100, result);
+        }
+    }
+}
