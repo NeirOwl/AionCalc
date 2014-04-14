4bbf17336fcc4fc9d2b26deca9a11a78947b87c4
aionforever <Tujh95@yandex.ru>
2014-04-14 19:06:32 +0700

    Added Tan.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Tan.Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Tan
+    {
+        [Test]
+        public void TanTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("tan");
+            double result = calc.Calculate(1);
+            Assert.AreEqual(1.55, result, 0.008);
+        }
+    }
+}
