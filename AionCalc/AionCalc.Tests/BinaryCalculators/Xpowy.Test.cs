66bd2425a919e79840d36bd77700b68cf08ff0e7
aionforever <Tujh95@yandex.ru>
2014-04-14 19:13:47 +0700

    Added Xpowy.Test
    and deleted Tenpowerx.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Xpowy.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Xpowy
+    {
+        [Test]
+        public void XpowyTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("x^y");
+            double result = calc.Calculate(3, 3);
+            Assert.AreEqual(27, result);
+        }
+    }
+}
