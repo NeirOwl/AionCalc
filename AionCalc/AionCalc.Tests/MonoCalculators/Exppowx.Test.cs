a173c83dcf8fad6118e07b6f44505e47b5c40b49
aionforever <Tujh95@yandex.ru>
2014-04-14 19:23:48 +0700

    Added Exppowx.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Exppowx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Exppowx
+    {
+        [Test]
+        public void ExppowxTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("e^x");
+            double result = calc.Calculate(2);
+            Assert.AreEqual(7.38, result, 0.01);
+        }
+    }
+}
