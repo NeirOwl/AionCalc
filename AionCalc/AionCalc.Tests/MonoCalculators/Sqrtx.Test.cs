e6c49e4ac00dd0a228b8e19a0b43a80c970fea05
aionforever <Tujh95@yandex.ru>
2014-04-14 18:17:21 +0700

    Added Sqrtx.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Sqrtx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Sqrtx
+    {
+        [Test]
+        public void AddTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("sqrtx");
+            double result = calc.Calculate(4);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
