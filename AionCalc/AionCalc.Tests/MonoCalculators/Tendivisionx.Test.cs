a4cb617d350a3d001e788f0e80be7ec5f7b51c87
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 17:57:36 +0700

    Added Tendivisionx.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Tendivisionx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Tendivisionx
+    {
+        [Test]
+        public void TendivisionxTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("10/x");
+            double result = calc.Calculate(5);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
