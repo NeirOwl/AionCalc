630659d3e35506f99a8cd001a6cec474347b75bb
FIFA-Forever <winunited@rambler.ru>
2014-04-15 13:50:08 +0700

    Added Tanh.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Tanh.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Tanh
+    {
+        [Test]
+        public void TanhTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("tanh");
+            double result = calc.Calculate(1);
+            Assert.AreEqual(0.76, result, 0.01);
+        }
+    }
+}
