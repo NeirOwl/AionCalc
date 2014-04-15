a2b6ec331b2162bca15a12d0e6e08290da77d249
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 17:50:39 +0700

    Added Sqrtpow3x.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Sqrtpow3x.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Sqrtpow3x
+    {
+        [Test]
+        public void Sqrtpow3xTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("sqrtpow3x");
+            double result = calc.Calculate(8);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
