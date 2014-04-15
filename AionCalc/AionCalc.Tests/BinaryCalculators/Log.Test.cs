a1b351e9bcaf81330da5b0e4e4cbc0a28147242a
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:05:01 +0700

    Added Log.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Log.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Log
+    {
+        [Test]
+        public void LogTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("log");
+            double result = calc.Calculate(3, 4);
+            Assert.AreEqual(0.79, result, 0.01);
+        }
+    }
+}
