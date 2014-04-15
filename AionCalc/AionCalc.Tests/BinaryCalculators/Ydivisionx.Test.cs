de63e6d229dfd4df37e1b42258c8769069c8d0a3
FIFA-Forever <winunited@rambler.ru>
2014-04-15 13:54:44 +0700

    Added Ydivisionx.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Ydivisionx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Ydivisionx
+    {
+        [Test]
+        public void YdivisionxTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("y/x");
+            double result = calc.Calculate(3, 6);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
