b233b4da13df2fbe04fee36903c5b61e468b9047
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 18:01:17 +0700

    Added Xpow3.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Xpow3.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Xpow3
+    {
+        [Test]
+        public void Xpow3Test()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("x^3");
+            double result = calc.Calculate(3);
+            Assert.AreEqual(27, result);
+        }
+    }
+}
