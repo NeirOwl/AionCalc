2851d26bd2d6d4f9f891863878e6f3e4e56e092f
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:12:45 +0700

    Added Cosh.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Cosh.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Cosh
+    {
+        [Test]
+        public void CoshTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("cosh");
+            double result = calc.Calculate(3);
+            Assert.AreEqual(10.06, result, 0.01);
+        }
+    }
+}
