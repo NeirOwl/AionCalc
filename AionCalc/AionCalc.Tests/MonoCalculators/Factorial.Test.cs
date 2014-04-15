5e300d0976e588083533d9ac3b5ce3a4d161e8f4
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 17:56:05 +0700

    Added Factorial.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Factorial.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Factorial
+    {
+        [Test]
+        public void FactorialTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("x!");
+            double result = calc.Calculate(5);
+            Assert.AreEqual(20, result);
+        }
+    }
+}
