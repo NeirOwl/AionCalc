bfca72d52a30c0a641ac67d49829f55f0ca1398d
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 17:59:29 +0700

    Added Tenmultiplyxdivisiony.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Tenmultiplyxdivisiony.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Tenmultiplyxdivisiony
+    {
+        [Test]
+        public void TenmultiplyxdivisionyTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("10*x/y");
+            double result = calc.Calculate(2, 4);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
