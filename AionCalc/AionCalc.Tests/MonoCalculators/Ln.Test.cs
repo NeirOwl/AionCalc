1e31bc795ed65e82c36f668baecf6f6b0edd9313
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:23:35 +0700

    Added Ln.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Ln.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Ln
+    {
+        [Test]
+        public void LnTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("ln");
+            double result = calc.Calculate(2);
+            Assert.AreEqual(0.69, result, 0.01);
+        }
+    }
+}
