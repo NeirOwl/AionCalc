aad990d40c996a4aeca4ff633be88b261328a705
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:20:23 +0700

    Added Lg.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Lg.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Lg
+    {
+        [Test]
+        public void LgTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("lg");
+            double result = calc.Calculate(3);
+            Assert.AreEqual(0.47, result, 0.01);
+        }
+    }
+}
