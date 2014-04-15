6638c4840f9a6a86a602896f870226a04cd14725
FIFA-Forever <winunited@rambler.ru>
2014-04-15 13:57:07 +0700

    Added Tenmultiplyx.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Tenmultiplyx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Tenmultiplyx
+    {
+        [Test]
+        public void TenmultiplyxTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("10*x");
+            double result = calc.Calculate(2);
+            Assert.AreEqual(20, result);
+        }
+    }
+}
