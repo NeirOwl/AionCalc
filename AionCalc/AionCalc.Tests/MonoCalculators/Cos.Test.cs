29afa5db8c8638d1b6e0b51ea343b263c71bd5ef
FIFA-Forever <winunited@rambler.ru>
2014-04-15 13:43:28 +0700

    Added Cos.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Cos.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Cos
+    {
+        [Test]
+        public void CosTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("cos");
+            double result = calc.Calculate(1);
+            Assert.AreEqual(0.53, result, 0.001);
+        }
+    }
+}
