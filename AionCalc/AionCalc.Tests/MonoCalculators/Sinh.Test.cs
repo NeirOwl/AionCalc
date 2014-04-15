427dd816d77505581fb17a5c12adee062f540dcd
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:08:46 +0700

    Added Sinh.Test

---
+++ AionCalc/AionCalc.Tests/MonoCalculators/Sinh.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.MonoCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.MonoCalculators
+{
+    [TestFixture]
+    public class Sinh
+    {
+        [Test]
+        public void SinhTest()
+        {
+            IMonoCalculator calc = MonoOperationsFactory.CreateCalculator("sinh");
+            double result = calc.Calculate(2);
+            Assert.AreEqual(3.62, result, 0.01);
+        }
+    }
+}
