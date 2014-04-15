45da1fcb2e0b71322db2394f091bde6589cf9b7c
Osipov944 <lexus_7456951@mail.ru>
2014-04-15 17:54:04 +0700

    Added Sqrtpowyx.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Sqrtpowyx.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Sqrtpowyx
+    {
+        [Test]
+        public void SqrtpowyxTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("sqrtpowyx");
+            double result = calc.Calculate(16, 4);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
