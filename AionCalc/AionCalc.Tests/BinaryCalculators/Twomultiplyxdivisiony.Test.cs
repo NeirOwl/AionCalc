790b04faf71ec6ef17ed9182dc572a82413134ba
likeaboss228 <ilikeaboss228@gmail.com>
2014-04-15 14:17:30 +0700

    Added Twomultiplyxdivisiony.Test

---
+++ AionCalc/AionCalc.Tests/BinaryCalculators/Twomultiplyxdivisiony.Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.BinaryCalculators;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.BinaryCalculators
+{
+    [TestFixture]
+    public class Twomultiplyxdivisiony
+    {
+        [Test]
+        public void TwomultiplyxdivisionyTest()
+        {
+            IBinaryCalculator calc = BinaryOperationsFactory.CreateCalculator("2*x/y");
+            double result = calc.Calculate(3, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
