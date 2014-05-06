b5311ccc7c17bbfee47a74f3587a01461f5af69c
likeaboss228 <ilikeaboss228@gmail.com>
2014-05-06 13:36:35 +0700

    Added SortBubble.Test

--- AionCalc/AionCalc.Tests/Sorting/SortBubble.Test.cs
+++ AionCalc/AionCalc.Tests/Sorting/SortBubble.Test.cs
@@ -9,12 +9,12 @@ using NUnit.Framework;
 namespace AionCalc.Tests.Sorting
 {
     [TestFixture]
-    public class SortInsertion
+    public class SortBubble
     {
         [Test]
-        public void SortInsertionTest()
+        public void SortBubbleTest()
         {
-            ISortingOperation sorter = SortingOperationFactory.CreateSorting("SortInsertion");
+            ISortingOperation sorter = SortingOperationFactory.CreateSorting("SortBubble");
             int[] source = { 5, 4, 3, 2, 1 };
             int[] expexted = { 1, 2, 3, 4, 5 };
             int[] result = sorter.SortMass(source);
