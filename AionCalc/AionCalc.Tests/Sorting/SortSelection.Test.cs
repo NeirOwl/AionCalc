863427d727f1c3540644be8e336180847dff301c
Osipov944 <lexus_7456951@mail.ru>
2014-05-06 13:42:22 +0700

    Added SortSelection.Test

---
+++ AionCalc/AionCalc.Tests/Sorting/SortSelection.Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AionCalc.Sorting;
+using NUnit.Framework;
+
+namespace AionCalc.Tests.Sorting
+{
+    [TestFixture]
+    public class SortSelection
+    {
+        [Test]
+        public void SortSelectionTest()
+        {
+            ISortingOperation sorter = SortingOperationFactory.CreateSorting("SortSelection");
+            int[] source = { 5, 4, 3, 2, 1 };
+            int[] expexted = { 1, 2, 3, 4, 5 };
+            int[] result = sorter.SortMass(source);
+            Assert.AreEqual(expexted.Length, result.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(expexted[i], result[i]);
+            }
+        }
+    }
+}
